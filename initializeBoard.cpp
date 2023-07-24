b22289b397664ad82a9e5d501ad57296217111b9
shristiGoswami <140273732+shristiGoswami@users.noreply.github.com>
2023-07-24 12:01:14 +0530

    initializeBoard.cpp

---
+++ initializeBoard.cpp
@@ -0,0 +1,8 @@
+// Function to initialize the game board with zeros
+void initializeBoard(int board[][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = 0;
+        }
+    }
+}
